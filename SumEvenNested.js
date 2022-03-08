66a824fdf5be9fbb57077abd154af96310a3f99c
Nathaniel Conrad <nathanielrconrad@gmail.com>
2022-03-07 23:10:16 -0500

    add first nested objects algo

---
+++ SumEvenNested.js
@@ -0,0 +1,67 @@
+function nestedEvenSum (object) {
+
+  let sum = 0;
+
+//base case if object is iterated through, return sum of evens 
+//open object, if value is another object recursively continue, if not an object check if remainder 2 equals 0
+//if so add to sum
+//recursively make your way through all nested objects in object
+
+  for (var key in object){
+
+    if(typeof(object[key]) === 'object'){
+      sum += nestedEvenSum(object[key]);
+    } else if (typeof(object[key]) === 'number' && object[key] % 2 === 0){
+      sum += object[key];
+    }
+  }
+  return sum;
+}
+
+
+var obj1 = {
+  outer: 2,
+  obj: {
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: "yup"
+    }
+  }
+}
+
+var obj2 = {
+  a: 2,
+  b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+  c: {c: {c: 2}, cc: 'ball', ccc: 5},
+  d: 1,
+  e: {e: {e: 2}, ee: 'car'}
+};
+
+nestedEvenSum(obj1); // 6
+nestedEvenSum(obj2); // 10
+
+
+var obj1 = {
+  outer: 2,
+  obj: {
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: "yup"
+    }
+  }
+}
+
+var obj2 = {
+  a: 2,
+  b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+  c: {c: {c: 2}, cc: 'ball', ccc: 5},
+  d: 1,
+  e: {e: {e: 2}, ee: 'car'}
+};
+
+nestedEvenSum(obj1); // 6
+nestedEvenSum(obj2); // 10
