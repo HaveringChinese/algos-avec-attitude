7ba9e76b38be5e049c843c45b4533d904149fe13
Nathaniel Conrad <nathanielrconrad@gmail.com>
2022-03-14 14:58:26 -0400

    complete mergeSort

---
+++ MergeSort.js
@@ -0,0 +1,66 @@
+//sort array by breaking down into two sorted arrays and then merging them
+// first stage of merge sort algorithm is to write a function that merges two sorted arrays into one sorted array
+
+function merge(arr1, arr2){
+
+  let newArr = [];
+  let i = 0;
+  let j = 0;
+  
+  while(i < arr1.length && j < arr2.length){
+
+      if(arr1[i] < arr2[j]){
+          newArr.push(arr1[i]);
+          i++;
+      } else if (arr2[j] < arr1[i]){
+          newArr.push(arr2[j]);
+          j++;
+      } else if (arr1[i] === arr2[j]){
+          newArr.push(arr1[i], arr2[j]);
+          i++;
+          j++;
+      }
+  }
+  console.log("before the last items: ", newArr);
+  
+  if(i < arr1.length){ 
+      newArr = newArr.concat(arr1.slice(i));
+  } else if (j < arr2.length){
+      newArr = newArr.concat(arr2.slice(j));
+  }
+  console.log("with the last items: ")
+  return newArr;
+}
+
+// *******first attempt at merge sort array split
+function mergeSort(arr){
+
+  let fhalf = [];
+  let shalf = [];
+  let mid = Math.floor(arr.length / 2)
+
+  if(arr.length <= 1) return arr; 
+
+      fhalf = mergeSort(arr.slice(0, mid));
+      shalf = mergeSort(arr.slice(mid));
+      return merge(fhalf, shalf);
+}
+
+mergeSort([15, 9, 14, 19, 17]);
+
+
+// mergeSorted([1,2,5,10,11], [3,4,6,8,20,21]);
+// mergeSorted([100], [2,3,4,5]);
+
+
+
+// function mergeSort(arr){
+
+//     //base case return array when length is less than or equal to 1
+//     //recursively split arrays into smaller chunks until base case satisfied
+//     if(arr.length <= 1) return arr;
+  
+//     return mergeSort(arr.splice(0, Math.floor(arr.length / 2)));    
+  
+  
+// }
