cf44df1de58a728bf2cb68e9b58db6a5309af499
Nathaniel Conrad <nathanielrconrad@gmail.com>
2022-03-19 14:48:10 -0400

    add radix rewrite

---
+++ RadixSortPractice.js
@@ -0,0 +1,41 @@
+//find digits
+//find longest digits
+//put into buckets and then back into array based on index of each digit
+
+// function getDigit(num, place){
+//     let reversed = [];
+//     let stringed = num.toString();
+//     for(let i = stringed.length; i > 0; i--){
+//         reversed.push(stringed[i]);
+//     }    
+//     console.log(reversed);
+//     reversed.join();
+//     return parseInt(reversed[place]);
+// } 
+
+const getDigitCleanly = (num, place) => Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+
+const digitCount = (num) => num.toString().length; 
+
+function maxDigits(array){
+    let max = 0;
+    for(let i = 0; i < array.length; i++){
+        max = Math.max(max, digitCount(array[i]));
+    }
+    return max;
+}
+
+function radixSort(array){
+
+    for(let k = 0; k < maxDigits(array); k++){
+        let buckets = Array.from({length: 10}, () => []);
+        for(let i = 0; i < array.length; i++){
+            let currentDigit = getDigitCleanly(array[i], k);
+            buckets[currentDigit] = array[i];
+        }
+        array = buckets.flat()
+    }
+    return array;
+}
+
+radixSort([1415,2,34234,33,55,12456236,2423143,4524,52343,23342]);
