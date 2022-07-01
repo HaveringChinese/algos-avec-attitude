8fa3e4cdbced3afb087dac1d0eedca10faabe60c
Nathaniel Conrad <nathanielrconrad@gmail.com>
2022-07-01 13:13:59 -0400

    first stab at LC subsequence checker

---
+++ isSubsequence.js
@@ -0,0 +1,12 @@
+//complete later
+var isSubsequence = function(s, t) {
+  for(let i = 0; i < t.length; i++){
+      if(t[i] === s[0]){
+          
+          break;
+      }
+      if(i === t.length) return false;
+  }
+  for(let j = 
+};
+
