70cde8f09afa56940e238d0681305dd663b28a99
NRC BeijingRedPC <nrconrad13@gmail.com>
2022-07-13 14:39:20 -0400

    add reverse SLL alg

---
+++ ReverseSLL.js
@@ -0,0 +1,33 @@
+//next stores what second points to
+//second points to head, stored in previous variable
+//pointer moves to second, repeat
+
+// var reverseList = function(head) {
+//   let current = head, prev = null, next;
+
+//   while(current){
+//       current = head.next;
+//       next = current.next;
+//       current.next = prev;
+//       prev.next = null;
+//       current = next;
+//   }
+//   return prev;
+// };
+
+//still need to recreate to demonstrate understanding!
+// Runtime: 119 ms, faster than 14.89% of JavaScript online submissions for Reverse Linked List.
+// Memory Usage: 44 MB, less than 81.15% of JavaScript online submissions for Reverse Linked List.
+var reverseList = function(head){
+  var current = head, prev = null, next;
+
+  while(current){
+      next = current.next; //next binds the node after current [1C,2N,3,4,5]
+      current.next = prev; // current points to null (tail, effectively) [null<-1C,2N,3,4,5]
+      prev = current; //previous now goes from null/prev to current node [1CP,2N,3,4,5]
+      current = next; //current node now goes to next (prep next for next.next) [1P,2CN,3,4,5]
+  }
+  return prev;
+
+}
+
