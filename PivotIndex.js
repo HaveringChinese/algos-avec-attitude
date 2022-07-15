f7cf6f77e7d2b2bbdfb4bda5a6ae17be695b4c48
NRC BeijingRedPC <nrconrad13@gmail.com>
2022-07-15 16:39:19 -0400

    second stab at algo 14-day challenge pivot index

--- PivotIndex.js
+++ PivotIndex.js
@@ -41,3 +41,16 @@ var pivotIndex = function(nums) {
   }
   return -1;
 };
+
+//gave this another stab and I understand it better; problem-solving skills improving!
+var pivotIndex = function(nums) {
+  let sum = 0, leftSum = 0;
+  
+  for(let i = 0; i < nums.length; i++) sum += nums[i];
+  
+  for(let j = 0; j < nums.length; j++){
+      if(leftSum === sum - leftSum - nums[j]) return j;
+      leftSum += nums[j];
+  }
+  return -1;
+};
