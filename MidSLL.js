fb01bf61c71340f1e6666f1a33cde8ab0c4731ef
NRC BeijingRedPC <nrconrad13@gmail.com>
2022-07-19 20:42:10 -0400

    make slight tweak to midsll for 2x speed

--- MidSLL.js
+++ MidSLL.js
@@ -16,3 +16,19 @@ var middleNode = function(head) {
     }
     return location;
 };
+
+//twice as fast! just as small!
+// Runtime: 68 ms, faster than 82.79% of JavaScript online submissions for Middle of the Linked List.
+// Memory Usage: 42 MB, less than 63.46% of JavaScript online submissions for Middle of the Linked List.
+var middleNode = function(head) {
+  let length = 0, current = head;
+  while(current){
+      current = current.next;
+      length++;
+  }
+  current = head;
+  for(let i = 0; i < Math.floor(length / 2); i++){
+      current = current.next;
+  }
+  return current;
+};
